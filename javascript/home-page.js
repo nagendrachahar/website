9c41d076b4284c65477e67e4a3f67b98fbfa2f8d
Your Name <you@example.com>
2016-12-21 20:31:32 +0530

    fil updated

--- javascript/home-page.js
+++ javascript/home-page.js
@@ -4,71 +4,141 @@ function myFunction(){
     var firstP = document.getElementById("firstP");
     var lastP = document.getElementById("lastP");
     var list = document.getElementById("list");
-	console.log(document.body.scrollTop);
-	if(document.body.scrollTop > 100){
+	console.log(document.documentElement.scrollTop);
+	if(document.documentElement.scrollTop > 100 || document.body.scrollTop > 100){
 		Name.className = "hidden";
 	} else{
 		Name.className = "visible";
 	}
 
-	if(document.body.scrollTop > 200){
+	if(document.documentElement.scrollTop > 200 || document.body.scrollTop > 200){
 		firstP.className = "hidden";
 	}else{
 		firstP.className = "visible";
 	}
 
-    if(document.body.scrollTop > 300){
+    if(document.documentElement.scrollTop > 300 || document.body.scrollTop > 300){
     	lastP.className = "hidden";
     }else{
     	lastP.className = "visible";
     }
 
-	if (document.body.scrollTop > 625) {
+	if (document.documentElement.scrollTop > 625 || document.body.scrollTop > 625) {
 		list.style.backgroundColor = "rgba(0,0,0,1)";
 		list.style.boxShadow = "0" + " " + "5px" + " " + "15px" + " " + "gray";
 	}else {
 		list.style.backgroundColor = "rgba(0,0,0,0)";
 		list.style.boxShadow = "none";
 	}
-	document.getElementById("opo").style.backgroundPosition =  "50%" + " " + -document.body.scrollTop * 0.2 + "px";
+	var scrollBackground = document.body.scrollTop + document.documentElement.scrollTop;
+
+	document.getElementById("mainUpperSection").style.backgroundPosition = "50%" + " " + -scrollBackground * 0.2 + "px";
+	
 	
 }
-window.onscroll = function() {myFunction()};
-
-function scrollPort(){
-	
+window.onscroll = function() {
+	myFunction();
+	progress();
 }
-
+function scrollPortfolio() {
+        if(document.documentElement.scrollTop > 550 || document.body.scrollTop > 550){
+		    var scrolltop = document.documentElement.scrollTop + document.body.scrollTop;
+		    var id = setInterval(frame, 1);
+            function frame(){
+		        if (scrolltop < 550) {
+		            clearInterval(id);
+		        }
+		        else {
+		            scrolltop = scrolltop - 5; 
+		            document.documentElement.scrollTop = scrolltop; 
+		            document.body.scrollTop = scrolltop;
+		        }
+		    }
+        }
+		else{
+			var scrolltop = document.documentElement.scrollTop + document.body.scrollTop;
+			var id = setInterval(frame, 1);
+		  	function frame(){
+		    	if (scrolltop > 550) {
+		            clearInterval(id);
+		        }
+		        else {
+		            scrolltop = scrolltop + 5; 
+		            document.documentElement.scrollTop = scrolltop; 
+		            document.body.scrollTop = scrolltop;
+		        }
+		    }
+		}
+}
+document.getElementById("portfolioButton").addEventListener("click", function(){
+	scrollPortfolio();
+});
 
 function scrollWork() {
-        if(document.body.scrollTop > 900){  
-		    var scrolltop = document.body.scrollTop;
-		    var id = setInterval(frame, 10);
+        if(document.documentElement.scrollTop > 900 || document.body.scrollTop > 900){  
+		    var scrolltop = document.documentElement.scrollTop + document.body.scrollTop;
+		    var id = setInterval(frame, 1);
             function frame(){
 		        if (scrolltop < 900) {
 		            clearInterval(id);
 		        }
 		        else {
-		            scrolltop = scrolltop - 10; 
-		            document.body.scrollTop = scrolltop; 
+		            scrolltop = scrolltop - 5; 
+		            document.documentElement.scrollTop = scrolltop; 
+		            document.body.scrollTop = scrolltop;
 		        }
-		    } 
+		    }
         }
 		else{
-			var scrolltop = document.body.scrollTop;
-			var id = setInterval(frame, 10);
+			var scrolltop = document.documentElement.scrollTop + document.body.scrollTop;
+			var id = setInterval(frame, 1);
 		  	function frame(){
 		    	if (scrolltop > 900) {
 		            clearInterval(id);
 		        }
 		        else {
-		            scrolltop = scrolltop + 10; 
-		            document.body.scrollTop = scrolltop; 
+		            scrolltop = scrolltop + 5; 
+		            document.documentElement.scrollTop = scrolltop; 
+		            document.body.scrollTop = scrolltop;
 		        }
 		    }
 		}
 }
 document.getElementById("workButton").addEventListener("click", function(){
 	scrollWork();
+});
+
+function scrollAbout() {
+        if(document.documentElement.scrollTop > 1250 || document.body.scrollTop > 1250){  
+		    var scrolltop = document.documentElement.scrollTop + document.body.scrollTop;
+		    var id = setInterval(frame, 1);
+            function frame(){
+		        if (scrolltop < 1250) {
+		            clearInterval(id);
+		        }
+		        else {
+		            scrolltop = scrolltop - 5; 
+		            document.documentElement.scrollTop = scrolltop; 
+		            document.body.scrollTop = scrolltop;
+		        }
+		    }
+        }
+		else{
+			var scrolltop = document.documentElement.scrollTop + document.body.scrollTop;
+			var id = setInterval(frame, 1);
+		  	function frame(){
+		    	if (scrolltop > 1250) {
+		            clearInterval(id);
+		        }
+		        else {
+		            scrolltop = scrolltop + 5; 
+		            document.documentElement.scrollTop = scrolltop; 
+		            document.body.scrollTop = scrolltop;
+		        }
+		    }
+		}
+}
+document.getElementById("aboutButton").addEventListener("click", function(){
+	scrollAbout();
 });
 
---
+++ jquery/home-page.js
@@ -0,0 +1,10 @@
+function progress(){
+	if(document.documentElement.scrollTop > 1250 || document.body.scrollTop > 1250){
+		console.log(document.body.scrollTop);
+$(document).ready(function() {
+       
+        $(".status-bar").animate({width:"400px"}, 2000);
+	});
+
+}
+}
